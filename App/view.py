72e53f91fced6cb257977dcd3693f93bdd6b1648
marianadiaz179 <m.diaza2@uniandes.edu.co>
2021-02-19 00:04:18 -0500

    Entrega Ejemplo – laboratorio 4”

--- App/view.py
+++ App/view.py
@@ -122,3 +122,13 @@ while True:
     else:
         sys.exit(0)
 sys.exit(0)
+
+def printResults(ord_books, sample=10): 
+    size = lt.size(ord_books)
+    if size > sample:
+        print("Los primeros ", sample, " libros ordenados son:") 
+        i=0
+        while i <= sample:
+            book = lt.getElement(ord_books,i) 
+            print('Titulo: ' + book['title'] + ' ISBN: ' + book['isbn'] + ' Rating: ' + book['average_rating']) 
+            i+=1
